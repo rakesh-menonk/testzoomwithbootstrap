6befbda8b7150e23e0ac548bec0564f995d756f3
Rakesh Menon K <rakesh.menonk@bestbuy.com>
2017-09-20 11:53:35 +0530

    Responsive changes

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
 console.log('main.js');
+$(window).resize(function() {
+    console.log('Window Resize width,height=', $(window).width(), ',', $(window).height());
+    console.log('Window Resize innerWidth,innerHeight', $(window).innerWidth(), ',', $(window).innerHeight());
+    console.log('Window Resize outerWidth,outerHeight', $(window).outerWidth(), ',', $(window).outerHeight());
+});
